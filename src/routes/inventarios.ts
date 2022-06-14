243722aaecf22dccbbc9f6c8a4d187f29837ae47
Diego Andres Polo Rada <aprendiz.financiera@mdosas.com>
2022-06-14 15:18:51 -0500

    fix: metodos de crud

--- src/routes/inventarios.ts
+++ src/routes/inventarios.ts
@@ -1,26 +1,39 @@
-import { Router } from "express";
-import { listInfo, saveInfo, updateInfo, deleteInfo } from "../utils/crud";
+import { Router } from 'express'
 
-import { inventarioModel } from "../model/inventarios";
+import { responseHTTPApi } from '../utils/request'
+import { inventarioModel } from '../model/inventarios'
+import {
+  listData,
+  saveData,
+  updateData,
+  deleteData
+} from '../services/crud'
 
 export const routeInventarios = Router()
 
-//listar
-routeInventarios.get('/', async(_req, res)=>{
-    listInfo(res, inventarioModel)
+// listar
+routeInventarios.get('/', (_req, res) => {
+  const data = listData(inventarioModel)
+  responseHTTPApi(res, 200, data)
 })
 
-//guardar
-routeInventarios.post('/', async(req, res)=>{
-    saveInfo(res, req, inventarioModel)
+// guardar
+routeInventarios.post('/', (req, res) => {
+  const data = req.body
+  const save = saveData(data, inventarioModel)
+  responseHTTPApi(res, 200, save)
 })
 
-//actualizar
-routeInventarios.put('/:id', async(req, res)=>{
-    updateInfo(req, res, inventarioModel)
+// actualizar
+routeInventarios.put('/:id', (req, res) => {
+  const { id } = req.params
+  const data = req.body
+  const update = updateData(id, data, inventarioModel)
+  responseHTTPApi(res, 200, update)
 })
 
-routeInventarios.put('/:id', async(req, res)=>{
-    deleteInfo(req, res, inventarioModel)
+routeInventarios.put('/:id', (req, res) => {
+  const { id } = req.params
+  const del = deleteData(id, inventarioModel)
+  responseHTTPApi(res, 200, del)
 })
-
