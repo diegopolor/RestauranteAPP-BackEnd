feb97ed0bfcfe0a8f9c5d0a5344d7ec371a43de2
DiegoPoloR <polodiego82@gmail.com>
2022-07-04 11:49:32 -0500

    fix: indentacion de model invetario

--- src/model/inventarios.ts
+++ src/model/inventarios.ts
@@ -1,19 +1,19 @@
-import {Schema, model} from "mongoose";
+import { Schema, model } from 'mongoose'
 
 const inventario = new Schema({
-    descripcion : "String",
-    cantidad : "Number"
+  descripcion: 'String',
+  cantidad: 'Number'
 })
 
 const registroInventario = new Schema({
-    fecha : "Date",
-    inventario : {
-        type : Schema.Types.ObjectId,
-        ref : 'inventario'
-    },
-    tipo : "String",
-    cantidad : "Number"
+  fecha: 'Date',
+  inventario: {
+    type: Schema.Types.ObjectId,
+    ref: 'inventario'
+  },
+  tipo: 'String',
+  cantidad: 'Number'
 })
 
 export const inventarioModel = model('inventario', inventario)
-export const registroInventarioModel = model('registroInventario',registroInventario)
+export const registroInventarioModel = model('registroInventario', registroInventario)
