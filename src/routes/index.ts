4503735aeadb10bc76088c61283116d1b5e3cf8f
DiegoPoloR <polodiego82@gmail.com>
2022-07-04 11:46:53 -0500

    prueba

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express'
 import { routeProductos } from './productos'
 import { routeCategoriaP } from './categoriaProductos'
-import { routeInventarios } from './inventarios'
+// import { routeInventarios } from './inventarios'
 
 export const routesApi = (app: any): void => {
   const routes = Router()
   routes.use('/productos', routeProductos)
   routes.use('/categorias', routeCategoriaP)
-  routes.use('/inventarios', routeInventarios)
+  // routes.use('/inventarios', routeInventarios)
   app.use('/api/v1/', routes)
 }
